fc9cfa372d91a4d2d1d4f76f207b52cf0a9df8b3
marcinmo <M.Mogiela@kainos.com>
2016-05-04 14:54:08 +0100

    added backLink factory

---
+++ public/scripts/backLink.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pleaApp')
+        .factory('backLink', backLink);
+
+    backLink.$inject = ['$rootScope', '$location'];
+
+    function backLink($rootScope, $location) {
+    	var history = [];
+        
+        $rootScope.$on('$routeChangeSuccess', updateHistory);
+        
+        return {
+          back: back;	
+        };
+
+        function back() {
+          var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+          return $location.path(prevUrl);
+        };
+
+        function updateHistory() {
+          if (history[history.length - 1] !== $location.$$path) {
+            history.push($location.$$path);
+          }
+        };
+    }
+})();
