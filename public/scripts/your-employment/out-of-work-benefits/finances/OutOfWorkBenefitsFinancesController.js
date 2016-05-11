5c112a10a1693fb00a46b3cd11e671fb746df560
marcinmo <M.Mogiela@kainos.com>
2016-05-11 11:42:42 +0100

    out-of-work-benefits route fixed

--- public/scripts/your-employment/out-of-work-benefits/finances/OutOfWorkBenefitsFinancesController.js
+++ public/scripts/your-employment/out-of-work-benefits/finances/OutOfWorkBenefitsFinancesController.js
@@ -5,13 +5,14 @@
   angular.module('pleaApp')
     .controller('OutOfWorkBenefitsFinancesController', OutOfWorkBenefitsFinancesController);
     
-  OutOfWorkBenefitsFinancesController.$inject = ['$scope', '$state'];  
+  OutOfWorkBenefitsFinancesController.$inject = ['$scope', '$state', '$sessionStorage'];  
     
-  function OutOfWorkBenefitsFinancesController($scope, $state) {
+  function OutOfWorkBenefitsFinancesController($scope, $state, $sessionStorage) {
 
     $scope.buttonContinue = function(event) { 
        
       event.preventDefault();
+      $sessionStorage.financialProblems = $scope.data.financialProblems;
       
       var nextState = $state.current.data.nextState;
       $state.go(nextState);
