e67a21f1c86afc830a0f7ee7af95bf2d29b21170
marcinmo <M.Mogiela@kainos.com>
2016-06-01 13:56:54 +0100

    Added spec for your-employment-finances controller

--- src/app/your-employment/your-employment-finances.controller.js
+++ src/app/your-employment/your-employment-finances.controller.js
@@ -5,84 +5,20 @@
   angular.module('pleaApp')
     .controller('YourEmploymentFinancesController', YourEmploymentFinancesController);
 
-  YourEmploymentFinancesController.$inject = ['yesNoAnswer', 'sessionStorage', 'state'];
+  YourEmploymentFinancesController.$inject = ['yesNoAnswer', 'yourEmploymentFinances', 'state'];
 
-  function YourEmploymentFinancesController(yesNoAnswer, sessionStorage, state) {
+  function YourEmploymentFinancesController(yesNoAnswer, yourEmploymentFinances, state) {
     var vm = this;
-
-    var BASE_NAME = 'pleaApp.yourEmployment.';
-
-    var get = sessionStorage.getGetter(BASE_NAME);
-    var set = sessionStorage.getSetter(BASE_NAME);
 
     vm.yesNoAnswer = yesNoAnswer;
     vm.buttonContinue = continueButtonClicked;
 
-    _updateViewModel();
-
-    // public
+    yourEmploymentFinances.updateVm(vm);
 
     function continueButtonClicked(event) {
       event.preventDefault();
-      _updateSessionStorage();
+      yourEmploymentFinances.updateSessionStorage(vm);
       state.goNext(vm);
-    }
-
-    // private
-
-    function _updateViewModel() {
-
-      vm.employmentStatus = get('employmentStatus');
-      vm.provideDetails = get('provideDetails');
-      vm.sourceIncome = get('sourceIncome');
-      vm.paymentFrequency = get('paymentFrequency');
-
-      if (vm.paymentFrequency === 'Weekly') {
-        vm.weeklyPay = get('paymentAmount');
-      } else if (vm.paymentFrequency === 'Fortnightly') {
-        vm.fortnightlyPay = get('paymentAmount');
-      } else if (vm.paymentFrequency === 'Monthly') {
-        vm.monthlyPay = get('paymentAmount');
-      } else if (vm.paymentFrequency === 'Other') {
-        vm.otherPay = get('paymentAmount');
-      }
-
-      vm.pensionCredit = get('pensionCredit');
-      vm.financialProblems = get('financialProblems');
-      vm.financialProblemsJustification = get('financialProblemsJustification');
-
-    }
-
-    function _updateSessionStorage() {
-
-      var employmentStatus = get('employmentStatus');
-
-      set('', undefined);
-      set('employmentStatus', employmentStatus);
-      set('provideDetails', vm.provideDetails);
-      set('sourceIncome', vm.sourceIncome);
-      set('paymentFrequency', vm.paymentFrequency);
-
-      if (vm.paymentFrequency === 'Weekly') {
-        set('paymentAmount', vm.weeklyPay);
-      } else if (vm.paymentFrequency === 'Fortnightly') {
-        set('paymentAmount', vm.fortnightlyPay);
-      } else if (vm.paymentFrequency === 'Monthly') {
-        set('paymentAmount', vm.monthlyPay);
-      } else if (vm.paymentFrequency === 'Other') {
-        set('paymentAmount', vm.otherPay);
-      }
-
-      set('pensionCredit', vm.pensionCredit);
-      set('financialProblems', vm.financialProblems);
-
-      if (vm.financialProblems === 'Yes') {
-        set('financialProblemsJustification', vm.financialProblemsJustification);
-      } else {
-        sessionStorage.reset('pleaApp.yourExpenses.');
-        sessionStorage.reset('pleaApp.yourBenefits.');
-      }
-
     }
   }
 })();
---
+++ src/app/your-employment/your-employment-finances.service.js
@@ -0,0 +1,73 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pleaApp')
+    .factory('yourEmploymentFinances', yourEmploymentFinances);
+
+  yourEmploymentFinances.$inject = ['sessionStorage'];
+
+  function yourEmploymentFinances(sessionStorage) {
+    var BASE_NAME = 'pleaApp.yourEmployment.';
+
+    var service = {
+      updateVm: updateVm,
+      updateSessionStorage: updateSessionStorage
+    };
+
+    return service;
+
+    function updateVm(vm) {
+      var get = sessionStorage.getGetter(BASE_NAME);
+
+      vm.employmentStatus = get('employmentStatus');
+      vm.provideDetails = get('provideDetails');
+      vm.sourceIncome = get('sourceIncome');
+      vm.paymentFrequency = get('paymentFrequency');
+
+      if (vm.paymentFrequency === 'Weekly') {
+        vm.weeklyPay = get('paymentAmount');
+      } else if (vm.paymentFrequency === 'Fortnightly') {
+        vm.fortnightlyPay = get('paymentAmount');
+      } else if (vm.paymentFrequency === 'Monthly') {
+        vm.monthlyPay = get('paymentAmount');
+      } else if (vm.paymentFrequency === 'Other') {
+        vm.otherPay = get('paymentAmount');
+      }
+
+      vm.pensionCredit = get('pensionCredit');
+      vm.financialProblems = get('financialProblems');
+      vm.financialProblemsJustification = get('financialProblemsJustification');
+    }
+
+    function updateSessionStorage(vm) {
+      var set = sessionStorage.getSetter(BASE_NAME);
+
+      set('', undefined);
+      set('employmentStatus', vm.employmentStatus);
+      set('provideDetails', vm.provideDetails);
+      set('sourceIncome', vm.sourceIncome);
+      set('paymentFrequency', vm.paymentFrequency);
+
+      if (vm.paymentFrequency === 'Weekly') {
+        set('paymentAmount', vm.weeklyPay);
+      } else if (vm.paymentFrequency === 'Fortnightly') {
+        set('paymentAmount', vm.fortnightlyPay);
+      } else if (vm.paymentFrequency === 'Monthly') {
+        set('paymentAmount', vm.monthlyPay);
+      } else if (vm.paymentFrequency === 'Other') {
+        set('paymentAmount', vm.otherPay);
+      }
+
+      set('pensionCredit', vm.pensionCredit);
+      set('financialProblems', vm.financialProblems);
+
+      if (vm.financialProblems === 'Yes') {
+        set('financialProblemsJustification', vm.financialProblemsJustification);
+      } else {
+        sessionStorage.reset('pleaApp.yourExpenses.');
+        sessionStorage.reset('pleaApp.yourBenefits.');
+      }
+    }
+  }
+})();
