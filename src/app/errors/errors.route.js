28084adb850fbbb0791241667ad368cd2b971e08
marcinmo <M.Mogiela@kainos.com>
2016-06-16 11:16:28 +0100

    Created error pages: 404 and 500

---
+++ src/app/errors/errors.route.js
@@ -0,0 +1,22 @@
+(function() {
+
+  'use strict';
+
+  angular.module('pleaApp')
+
+    .config(function($stateProvider) {
+
+      $stateProvider
+        .state('error-404', {
+          url: '/error404',
+          templateUrl: 'app/errors/404.html'
+        })
+
+        .state('error-500', {
+          url: '/error500',
+          templateUrl: 'app/errors/500.html'
+        });
+
+    });
+
+})();
