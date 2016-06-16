4e51a30fa9f29972d9a3ae8a80009b88b30aa5df
Shaks <s.khan@kainos.com>
2016-06-16 15:51:21 +0100

    Added your-case component

---
+++ src/app/views/plead-online/your-case/your-case.component.js
@@ -0,0 +1,19 @@
+(function() {
+  
+  'use strict';  
+    
+  angular
+    .module('pleaApp')
+    .component('yourCase' {
+      bindings: {},
+      controller: 'YourCaseController',
+      controllerAs: 'vm',
+      templateUrl: 'app/views/plead-online/your-case/your-case.tpl.html',
+    });
+    
+  function YourCaseController() {
+
+    var vm = this;
+
+  }
+})();
