2a7b8df6913110b3da0a5b38e27a9d15ec95a3f5
Shaks <s.khan@kainos.com>
2016-06-17 15:49:56 +0100

    Added a unit test for back link

--- src/app/components/common/back-link.component.spec.js
+++ src/app/components/common/back-link.component.spec.js
@@ -1,1 +1,27 @@
-back-link.component.spec.js
+ describe('Should take you to the previous page', function (){
+
+  var scope, elem, compiled;
+
+  beforeEach(module(
+    'pleaApp'
+  ));
+
+  beforeEach(inject(function ($injector) {
+    $window     = $injector.get('$window');
+    $compile    = $injector.get('$compile');
+    $rootScope  = $injector.get('$rootScope');
+    scope       = $rootScope.$new();
+    elem        = angular.element('<a class="link-back" href="" data-ng-click="vm.goBack()" title="Go back to previous page">Back</a>');
+    compiled    = $compile(elem);
+    compiled(scope);
+    scope.$digest();
+
+  }));
+
+  it('should call history.back when a back link is clicked', function () {
+    spyOn($window.history, 'back');
+    elem[0].click();
+    expect($window.history.back).toHaveBeenCalled();
+  });
+
+});
