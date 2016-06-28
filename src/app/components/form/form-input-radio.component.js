18414b0450cd2217ca7c6be8586ff140260b9374
Shaks <s.khan@kainos.com>
2016-06-28 09:08:10 +0100

    Using directive instead of a component

--- src/app/components/form/form-input-radio.component.js
+++ src/app/components/form/form-input-radio.component.js
@@ -1,33 +1,33 @@
-(function () {
+// (function () {
 
-  'use strict';
+//   'use strict';
 
-  angular
-    .module('pleaApp')
-    .component('formInputRadio', {
-      controller: FormInputRadioController,
-      controllerAs: 'vm',
-      templateUrl: 'app/components/form/form-input-radio.html',
-      bindings: {
-        name: '@',
-        value: '@',
-        form: '<',
-        property: '<',
-        onUpdate: '&'
-      }
-    });
+//   angular
+//     .module('pleaApp')
+//     .component('formInputRadio', {
+//       controller: FormInputRadioController,
+//       controllerAs: 'vm',
+//       templateUrl: 'app/components/form/form-input-radio.html',
+//       bindings: {
+//         name: '@',
+//         value: '@',
+//         form: '<',
+//         property: '<',
+//         onUpdate: '&'
+//       }
+//     });
 
-  function FormInputRadioController() {
-    var vm = this;
+//   function FormInputRadioController() {
+//     var vm = this;
 
-    vm.onClick = function(value) {
-      vm.onUpdate({value: value});
-    }
-  }
+//     vm.onClick = function(value) {
+//       vm.onUpdate({value: value});
+//     }
+//   }
 
-}());
+// }());
 
-/*
+
 (function() {
     'use strict';
     angular
@@ -60,4 +60,3 @@
     }
 
 })(); 
-*/
