1f41fba20e93baff6e9dbda1bbf23d93d964b59b
Shaks <s.khan@kainos.com>
2016-07-01 09:35:44 +0100

    Added landing page of make a plea application as a main component

---
+++ src/app/views/plead-online/main/main.component.js
@@ -0,0 +1,18 @@
+(function () {
+
+	'use strict';
+
+	angular
+	.module('pleaApp')
+	.component('mainComponent', {
+		bindings: {},
+		controller: mainController,
+		controllerAs: 'vm',
+		templateUrl: 'app/views/plead-online/main/main.tpl.html'
+	});
+
+	function mainController() {
+	  
+	}
+
+}());
