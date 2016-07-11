1a0524fb030bb910c542679e2aad979ad69a5133
Shaks <s.khan@kainos.com>
2016-07-11 14:13:35 +0100

    Added a new function responsible for redirecting the user based on the selection

--- src/app/states/confirm-your-answers/confirm-your-answers.controller.js
+++ src/app/states/confirm-your-answers/confirm-your-answers.controller.js
@@ -5,18 +5,28 @@
   angular.module('pleaApp')
     .controller('ConfirmYourAnswersController', ConfirmYourAnswersController);
     
-  ConfirmYourAnswersController.$inject = ['confirmYourAnswers', 'state'];
+  ConfirmYourAnswersController.$inject = ['confirmYourAnswers', 'state', '$state', '$filter'];
     
-  function ConfirmYourAnswersController(confirmYourAnswers, state) {
+  function ConfirmYourAnswersController(confirmYourAnswers, state, $state, $filter) {
     var vm = this;
 
     vm.buttonContinue = continueButtonClicked;
+    vm.goToSelection = goToSelection;
     
     confirmYourAnswers.updateVm(vm);
 
     function continueButtonClicked(event) {
       event.preventDefault();
       state.goNext();
+    }
+
+    function goToSelection () {
+      var url = $filter('replaceSpaces')(vm.pleaApp.yourEmployment.employmentStatus);
+      if(vm.pleaApp.yourEmployment.employmentStatus.indexOf('benefits') === -1) {
+        $state.go("your-employment." + url + ".finances");
+      } else {
+        $state.go("your-benefits");
+      }
     }
   }
 })();
