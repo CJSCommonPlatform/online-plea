7992829d89e9ffd5d63d9acd696ebd93d68d8ba6
Radoslaw Nadstawny <r.nadstawny@kainos.com>
2016-07-14 09:47:37 +0100

    Copied the structure service over from UI.App
    And added a method for making a plea.
    
    In the future, when online-plea is migrated
    to UI.App, these changes will have to be
    integrated into the UI.App code.

---
+++ src/app/components/common/does-not-really-belong-here/api-calls.service.js
@@ -0,0 +1,49 @@
+﻿(function () {
+
+  'use strict';
+
+  angular
+    .module('pleaApp')
+    .factory('apiCallsService', apiCallsService);
+
+  function apiCallsService($q, $http, globalConfig) {
+
+    var service = {
+      query: query,
+      command: command
+    };
+
+    return service;
+
+    function query(params) {
+      return $http({
+        method: 'GET',
+        url: globalConfig.apiRoot + params.endPoint,
+        headers: {
+          'Accept': params.mimeType
+        }
+      })
+      .catch(function (respError) {
+        respError.method = params.error;
+        return $q.reject(respError);
+      });
+    }
+
+    function command(params) {
+      return $http({
+        method: 'POST',
+        url: globalConfig.apiRoot + params.endPoint,
+        data: params.data,
+        headers: {
+          'Content-Type': params.mimeType
+        }
+      })
+      .catch(function (respError) {
+        respError.method = params.error;
+        return $q.reject(respError);
+      });
+    }
+
+  }
+
+}());
