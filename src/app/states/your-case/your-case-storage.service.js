f03ad16648576a5b301c0fc8447bab299e374d7b
Shaks <s.khan@kainos.com>
2016-07-19 12:45:25 +0100

    Added a structure service responsible for making a call to backend to fetch the data

--- src/app/states/your-case/your-case-storage.service.js
+++ src/app/states/your-case/your-case-storage.service.js
@@ -8,7 +8,7 @@
   yourCaseStorage.$inject = ['sessionStorage'];
 
   function yourCaseStorage(sessionStorage) {
-    var BASE_NAME = 'pleaApp.yourCase.';
+    var BASE_NAME = 'pleaApp.yourDetails.';
 
     var get = sessionStorage.getGetter(BASE_NAME);
     var set = sessionStorage.getSetter(BASE_NAME);
@@ -25,9 +25,12 @@
       vm.casePostcode = get('casePostcode');
     }
 
-    function updateSessionStorage(vm) {
-      set('caseUniqueReferenceNumber', vm.caseUniqueReferenceNumber);
-      set('casePostcode', vm.casePostcode.toUpperCase());
+    function updateSessionStorage(data) {
+      set('firstName', data.defendant.firstName);
+      set('lastName', data.defendant.lastName);
+      set('address.street', data.defendant.address.addressLine1);
+      set('address.city', data.defendant.address.addressLine2);
+      set('address.postcode', data.defendant.address.postCode.toUpperCase());
     }
 
   }
