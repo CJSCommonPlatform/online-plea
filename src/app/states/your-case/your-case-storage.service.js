680a4baec20b68a441097f61bf57098d80bc37f7
Shaks <s.khan@kainos.com>
2016-07-20 11:10:24 +0100

    DOB is now being populated from response we get from backend

--- src/app/states/your-case/your-case-storage.service.js
+++ src/app/states/your-case/your-case-storage.service.js
@@ -26,8 +26,13 @@
     }
 
     function updateSessionStorage(data) {
+      var dob = moment(data.defendant.dateOfBirth);
+
       set('firstName', data.defendant.firstName);
       set('lastName', data.defendant.lastName);
+      set('dateOfBirthDay', dob.date());
+      set('dateOfBirthMonth', dob.month() + 1);
+      set('dateOfBirthYear', dob.year());
       set('address.street', data.defendant.address.addressLine1);
       set('address.city', data.defendant.address.addressLine2);
       set('address.postcode', data.defendant.address.postCode.toUpperCase());
